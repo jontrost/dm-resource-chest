79deaa0bac23200a31b7fda32ee493b3e8fae1e1
jontrost <jontrost426@gmail.com>
2019-05-24 20:48:46 -0500

    Changed how each magic item post is displayed
    Changed how each magic item is displayed in the magic items page component by using the ngFor directive. Added basic CSS styling to magic item posts and populated real data into the magic items JSON file for testing purposes.

--- src/app/magic-items/magic-items-data.service.ts
+++ src/app/magic-items/magic-items-data.service.ts
@@ -18,10 +18,15 @@ export class MagicItemsDataService {
     let magicItemComponentsArray: MagicItemComponent[] = [];
     this.getJSONData().subscribe(function(value){
       for(let i = 0; i < value.posts.length; i++){
-        let component = new MagicItemComponent("title", "description", "type", "details");
+        let component = new MagicItemComponent(
+          value.posts[i].title, 
+          value.posts[i].description, 
+          value.posts[i].type, 
+          value.posts[i].details);
         magicItemComponentsArray.push(component);
       }
     });
+    
     return magicItemComponentsArray;
   }
 
