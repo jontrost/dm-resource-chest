594bcbd5a27ec93c605aec67df87f72bd3a9ed58
Jon Trost <jontrost426@gmail.com>
2019-06-28 17:57:06 -0500

    Updated svgs

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
-import { ContactRoutingModule } from './contact-routing.module';
-import { FormsModule }   from '@angular/forms';
-import { RecaptchaModule } from 'ng-recaptcha';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ContactComponent } from "./contact/contact.component";
+import { ContactRoutingModule } from "./contact-routing.module";
+import { FormsModule } from "@angular/forms";
+import { RecaptchaModule } from "ng-recaptcha";
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [
-    CommonModule,
-    ContactRoutingModule,
-    FormsModule,
-    RecaptchaModule
-  ]
+  imports: [CommonModule, ContactRoutingModule, FormsModule, RecaptchaModule]
 })
-export class ContactModule { }
+export class ContactModule {}
