179d8ac7d4c9533775071c4b8b8361700e72aeeb
Jon Trost <jontrost426@gmail.com>
2019-07-08 16:17:37 -0500

    Added filter functionality for npcs

--- src/app/npcs/npc-page/npc-page.component.ts
+++ src/app/npcs/npc-page/npc-page.component.ts
@@ -12,11 +12,114 @@ import { Observable } from 'rxjs';
 export class NpcPageComponent implements OnInit {
 
   posts$: Observable<any>;
+  filteredPosts$: Observable<any>;
+  rawPosts$: Observable<any>;
 
-  constructor(private npcsDataService: NpcsDataService) { }
+  showMale: string = 'male';
+  showFemale: string = 'female';
+  showYoung: string = 'young';
+  showMiddleAged: string = 'middle-aged';
+  showOld: string = 'old';
 
-  ngOnInit() {
-    this.posts$ = this.npcsDataService.getJSONData().pipe(map(posts => posts.posts));
+  constructor(private npcsDataService: NpcsDataService) {
+
   }
 
+  updateFilters() {
+    if (this.showYoung == '' && this.showMiddleAged == '' && this.showOld == '') {
+      this.posts$ = this.rawPosts$.pipe(
+        map(posts => posts.posts.filter(
+          post =>
+            post.sex == this.showMale || post.sex == this.showFemale
+          )
+        )
+      );
+    }
+    else if (this.showMale  == '' && this.showFemale == '') {
+      this.posts$ = this.rawPosts$.pipe(
+        map(posts => posts.posts.filter(
+          post =>
+            post.age == this.showYoung
+            || post.age == this.showMiddleAged
+            || post.age == this.showOld
+          )
+        )
+      );
+    }
+    else {
+      this.posts$ = this.rawPosts$.pipe(
+        map(posts => posts.posts.filter(
+          post =>
+            (
+            post.sex == this.showMale
+            || post.sex == this.showFemale
+            )
+            &&
+            (
+            post.age == this.showYoung
+            || post.age == this.showMiddleAged
+            || post.age == this.showOld
+            )
+          )
+        )
+      );
+    }
+  }
+
+  ngOnInit() {
+    this.rawPosts$ = this.npcsDataService.getJSONData();
+    this.posts$ = this.rawPosts$.pipe(
+      map(posts => posts.posts)
+    );
+  }
+
+  toggleMale() {
+    if (this.showMale == 'male') {
+      this.showMale = '';
+    }
+    else {
+      this.showMale = 'male';
+    }
+    this.updateFilters();
+  }
+
+  toggleFemale() {
+    if (this.showFemale == 'female') {
+      this.showFemale = '';
+    }
+    else {
+      this.showFemale = 'female';
+    }
+    this.updateFilters();
+  }
+
+  toggleYoung() {
+    if (this.showYoung == 'young') {
+      this.showYoung = '';
+    }
+    else {
+      this.showYoung = 'young';
+    }
+    this.updateFilters();
+  }
+
+  toggleMiddleAged() {
+    if (this.showMiddleAged == 'middle-aged') {
+      this.showMiddleAged = '';
+    }
+    else {
+      this.showMiddleAged = 'middle-aged';
+    }
+    this.updateFilters();
+  }
+
+  toggleOld() {
+    if (this.showOld == 'old') {
+      this.showOld = '';
+    }
+    else {
+      this.showOld = 'old';
+    }
+    this.updateFilters();
+  }
 }
