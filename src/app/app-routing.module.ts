1575ccc6e720e3deaeb6e2a08c924e9c36963576
jontrost <jontrost426@gmail.com>
2019-08-21 20:52:02 -0500

    updated to angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { HomeComponent } from "./home/home.component";
 const routes: Routes = [
 	{
 		path: "magic-items",
-		loadChildren: "./magic-items/magic-items.module#MagicItemsModule"
+		loadChildren: () => import('./magic-items/magic-items.module').then(m => m.MagicItemsModule)
 	},
-	{ path: "npcs", loadChildren: "./npcs/npcs.module#NpcsModule" },
+	{ path: "npcs", loadChildren: () => import('./npcs/npcs.module').then(m => m.NpcsModule) },
 	{
 		path: "plot-hooks",
-		loadChildren: "./plot-hooks/plot-hooks.module#PlotHooksModule"
+		loadChildren: () => import('./plot-hooks/plot-hooks.module').then(m => m.PlotHooksModule)
 	},
-	{ path: "contact", loadChildren: "./contact/contact.module#ContactModule" },
+	{ path: "contact", loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
 	{ path: "home", component: HomeComponent },
 	{ path: "", component: HomeComponent },
 	{ path: "**", redirectTo: "" }
